cd991a153f9912d624de214701d59dafe0a70503
Jairo Chapela <jairochapela@gmail.com>
2022-08-25 20:30:53 +0200

    Barco, Tablero

--- barco.py
+++ barco.py
@@ -1,32 +1,48 @@
-from fileinput import filename
-from tkinter import HORIZONTAL, VERTICAL
-
-
 class Barco:
 
+    # Estas constantes definen el avance de
+    # las casillas a ocupar por un barco según
+    # su orientación: horizontal, vertical.
     HORIZONTAL = (1,0)
     VERTICAL = (0,1)
 
-    def __init__(self, fila, columna, longitud, orientacion) -> None:
+    def __init__(self, fila, columna, longitud, orientacion):
+
+        horz,vert = orientacion
+
+        if fila not in range(0, 10-vert*longitud) or columna not in range(0, 10-horz*longitud):
+            raise ValueError("Posicion no válida")
+
         self.fila = fila
         self.columna = columna
         self.longitud = longitud
         self.orientacion = orientacion
     
-
+    # Cálculo de los impactos en el barco, dada
+    # la secuencia de disparos.
     def impactos(self, disparos):
-        v = self.orientacion
-        celdas = [(self.columna + v[0]*k, self.fila + v[1]*k) for k in range(self.longitud)]
-        return [d in celdas for d in disparos]
+        horz,vert = self.orientacion
+        celdas = [(self.columna + horz*k, self.fila + vert*k) 
+            for k in range(self.longitud)]
+        return [c in disparos for c in celdas]
 
-    def hundido(self):
-        return all(self.impactos())
+    # Un barco está hundido si todas sus posiciones
+    # fueron tocadas
+    def hundido(self, disparos):
+        return all(self.impactos(disparos))
 
-    def dibujar(self, t):
-        v = self.orientacion
-        for k in range(self.longitud):
-            x, y = self.columna + v[0]*k, self.fila + v[1]*k
-            t[x][y] = '@'
-
+    def dibujar(self, t, disparos):
+        pass
 
 
+
+
+
+
+if __name__ == '__main__':
+    b1 = Barco(fila=3, columna=5, longitud=4, orientacion=Barco.HORIZONTAL)
+    b2 = Barco(0, 2, 3, Barco.VERTICAL)
+    print(b1)
+    disparos = [(1,2), (4,5), (8,3)]
+    print(b1.impactos(disparos))
+    print(b1.hundido(disparos))
--- tablero.py
+++ tablero.py
@@ -3,7 +3,7 @@ from barco import Barco
 
 class Tablero:
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.posicionesBarcos = []
         self.disparos = []
 
@@ -12,34 +12,49 @@ class Tablero:
         #TODO: comprobar solapamiento
 
     def recibirDisparo(self, fila, columna):
-        if (fila, columna) in self.disparos:
+        if fila < 0 or fila >=10\
+        or columna < 0 or columna >= 10:
+            raise ValueError("Disparo fuera de rango")
+  
+        if (columna, fila) in self.disparos:
             raise ValueError("Disparo repetido")
-        self.disparos.append((fila, columna))
+        self.disparos.append((columna, fila))
+
 
     def hundidos(self):
-        pass
+        return filter(
+            lambda b: b.hundido(self.disparos),
+            self.posicionesBarcos)
+
 
     def dibujar(self, ocultarBarcos):
+        # 1. Crear matriz de 10x10
         t = [[' ' for x in range(10)] for y in range(10)]
+        # 2. Dibujar los disparos
+        for disparo in self.disparos:
+            x, y = disparo
+            t[x][y] = '*'
+        # 3. Colocar los barcos
         if not ocultarBarcos:
-            for b in self.posicionesBarcos:
-                b.dibujar(t)
-        for d in self.disparos:
-            fila, columna = d
-            t[fila][columna] = 'x' if t[fila][columna]!=' ' else 'o'
+            for barco in self.posicionesBarcos:
+                barco.dibujar(t, self.disparos)
+        # 4. Imprimir tablero
         s = "+---" * 10 + "+\n"
         for y in range(10):
             for x in range(10):
-                s += f"| {t[x][y]} "
-            s += "|\n" + ("+---" * 10) + "+\n"
+                s += "| " + t[x][y] + " "
+            s += "|\n"
+            s += "+---" * 10 + "+\n"
         print(s)
 
+
+
 if __name__ == '__main__':
-    t = Tablero()
-    b1 = Barco(2, 3, 4, Barco.HORIZONTAL)
-    b2 = Barco(5, 8, 2, Barco.VERTICAL)
-    t.posicionarBarco(b1)
-    t.posicionarBarco(b2)
-    t.recibirDisparo(2, 5)
-    t.recibirDisparo(5, 2)
-    t.dibujar(ocultarBarcos=False)
+    tab = Tablero()
+    b1 = Barco(fila=2, columna=3, longitud=4, orientacion=Barco.HORIZONTAL)
+    b2 = Barco(fila=5, columna=8, longitud=2, orientacion=Barco.VERTICAL)
+    tab.posicionarBarco(b1)
+    tab.posicionarBarco(b2)
+    tab.recibirDisparo(2, 5)
+    tab.recibirDisparo(5, 2)
+    tab.dibujar(ocultarBarcos=False)
