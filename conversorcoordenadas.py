b54076c2b58bd22133569fdcf5a533c9d2006f4d
Jairo Chapela <jairochapela@gmail.com>
2022-09-02 16:51:43 +0200

    Captura de excepción

--- conversorcoordenadas.py
+++ conversorcoordenadas.py
@@ -1,6 +1,12 @@
+from multiprocessing.sharedctypes import Value
+
+
 def convertir_coordenadas(spec):
     letras = "ABCDEFGHIJ"
     r = spec.lstrip().upper()
-    fila = letras.index(r[0])
+    try:
+        fila = letras.index(r[0])
+    except ValueError as err:
+        raise ValueError(f"Fila no reconocida: {err}")
     columna = int(r[1:]) - 1
     return fila, columna
